90c39af1af9cc24d030c987ae1a965bfd27a15d4
SadigAli <sadig.aliev99@gmail.com>
2021-10-28 01:06:48 +0400

    Tags and Categories CRUD

---
+++ Leka/Leka/Areas/manage/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using Leka.DAL;
+using Leka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leka.Areas.manage.Controllers
+{
+    [Area("manage")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid) return View();
+            if (_context.Categories.Any(x => x.Name.ToLower() == category.Name.ToLower()))
+            {
+                ModelState.AddModelError("Name", "Bu category artiq movcuddur");
+                return View();
+            }
+
+            _context.Categories.Add(new Category { Name = category.Name });
+            _context.SaveChanges();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Category category = _context.Categories.Find(id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int? id,Category category)
+        {
+            if (id == null) return NotFound();
+
+            Category categoryDb = _context.Categories.Find(id);
+            if (categoryDb == null) return NotFound();
+
+            if (_context.Categories.Any(x => x.Name.ToLower() == category.Name.ToLower() && x.Id != id))
+            {
+                ModelState.AddModelError("Name", "Bu category artiq movcuddur");
+                return View();
+            }
+
+            categoryDb.Name = category.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Category category = _context.Categories.Find(id);
+            if (category == null) return NotFound();
+            try
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return Json(new
+                {
+                    code = 204,
+                    message = "Item has been deleted successfully!"
+                });
+            }
+            catch (Exception)
+            {
+                return Json(new
+                {
+                    code = 500,
+                    message = "Something went wrong!"
+                });
+            }
+        }
+    }
+}
---
+++ Leka/Leka/Areas/manage/Controllers/TagController.cs
@@ -0,0 +1,106 @@
+﻿using Leka.DAL;
+using Leka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leka.Controllers
+{
+    [Area("manage")]
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Tag> Tags = _context.Tags.ToList();
+            return View(Tags);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Tag tag)
+        {
+            if (!ModelState.IsValid) return View();
+            if (_context.Tags.Any(x => x.Name.ToLower() == tag.Name.ToLower()))
+            {
+                ModelState.AddModelError("Name", "Bu tag artiq movcuddur");
+                return View();
+            }
+
+            _context.Tags.Add(new Tag { Name = tag.Name });
+            _context.SaveChanges();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Tag tag = _context.Tags.Find(id);
+            if (tag == null) return NotFound();
+
+            return View(tag);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int? id, Tag tag)
+        {
+            if (id == null) return NotFound();
+
+            Tag tagDb = _context.Tags.Find(id);
+            if (tagDb == null) return NotFound();
+
+            if (_context.Tags.Any(x => x.Name.ToLower() == tag.Name.ToLower() && x.Id != id))
+            {
+                ModelState.AddModelError("Name", "Bu tag artiq movcuddur");
+                return View();
+            }
+
+            tagDb.Name = tag.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Tag tag = _context.Tags.Find(id);
+            if (tag == null) return NotFound();
+            try
+            {
+                _context.Tags.Remove(tag);
+                _context.SaveChanges();
+                return Json(new
+                {
+                    code = 204,
+                    message = "Item has been deleted successfully!"
+                });
+            }
+            catch (Exception)
+            {
+                return Json(new
+                {
+                    code = 500,
+                    message = "Something went wrong!"
+                });
+            }
+        }
+    }
+}
